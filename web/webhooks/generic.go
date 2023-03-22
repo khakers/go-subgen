cd73dbcf6d676cc2c384fb2b2ec69059e29f316a
Khakers <22665282+khakers@users.noreply.github.com>
2023-03-21 20:35:55 -0700

    Imrpove generic endpoint debug logging and rename json key to files from file

--- web/webhooks/generic.go
+++ web/webhooks/generic.go
@@ -11,20 +11,25 @@ import (
 )
 
 type GenericWebhookData struct {
-	Files []string `json:"file"`
+	Files []string `json:"files"`
 }
 
 func ServeGeneric(w http.ResponseWriter, r *http.Request) {
+	log.Debugln("Received generic webhook")
+
 	var data GenericWebhookData
 	err := json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
-		log.Errorln(err)
-		w.WriteHeader(400)
-		fmt.Fprintf(w, "error: %v", err)
+		log.WithError(err).Errorln("Failed to decode webhook JSON data")
+
+		http.Error(w, fmt.Sprint(err), http.StatusBadRequest)
 		return
 	}
-	for _, s := range data.Files {
-		pkg.EnqueueSub(s)
+	log.WithField("data", fmt.Sprintf("%+v", data)).Debugln("Decoded webhook json data")
+
+	for _, file := range data.Files {
+		log.Debugf("Queued %v from radarr", file)
+		pkg.EnqueueSub(file)
 	}
 	w.WriteHeader(200)
 }
